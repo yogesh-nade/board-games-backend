fa520d355cf34ccb419098604672815843e8ce93
yogesh-nade <yogeshnade43@gmail.com>
2025-11-14 17:48:50 +0530

    TEMP: Add alias for dashboard typo to make deployment work

--- backend/board_games/wsgi.py
+++ backend/board_games/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'board_games.settings')
 
 application = get_wsgi_application()
+
+# TEMPORARY: Alias for Render typo - remove this after fixing dashboard
+applicatio = application
